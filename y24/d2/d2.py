c14504581e40a61f9b9e3aa4ac6c67133aaadd88
Bertil Varenhorst <bertilva@student.chalmers.se>
2024-12-10 18:14:47 +0100

    d2 solution

--- y24/d2/d2.py
+++ y24/d2/d2.py
@@ -1,2 +1,40 @@
 from aocd import data
-print(data)
+
+
+reports = [list(map(int, line.split())) for line in data.strip().split('\n')]
+
+# Part 1
+def check_safe(report):
+    if all(report[k] < report[k+1] for k in range(len(report) - 1)) or all(report[k] > report[k+1] for k in range(len(report) - 1)):
+        for n in range(0, len(report) - 1):
+            change = abs(report[n] - report[n+1])
+            if change > 3 or change < 1:
+                break
+        else: #if loop dont break
+            return True
+    return False
+
+safe = 0
+for report in reports:
+    if check_safe(report):
+        safe += 1
+print(f'Part 1: {safe}')
+
+# Part 2
+safe2 = 0
+for report in reports:
+    if not check_safe(report):
+        k = 0
+        while k < len(report):
+            removed = report.pop(k)
+            if not check_safe(report):
+                report.insert(k, removed) 
+            else:
+                safe2 += 1
+                break
+            k += 1
+    else:
+        safe2 += 1
+print(f'Part 2: {safe2}')
+
+    
