f967e63f6b37ad37f2cac6e7a57dbd78b6399e65
Bertil Varenhorst <bertilva@student.chalmers.se>
2024-12-12 16:07:22 +0100

    d6 solution

--- y24/d6/d6.py
+++ y24/d6/d6.py
@@ -3,12 +3,12 @@ map = [line.strip() for line in data.strip().split('\n')]
 
 test = """
 ....#.....
-....^....#
+.........#
 ..........
 ..#.......
 .......#..
 ..........
-.#........
+.#..^.....
 ........#.
 #.........
 ......#..."""
@@ -20,7 +20,7 @@ test = [line.strip() for line in test.strip().split('\n')]
 
 directions = { # key , [ current direction, next direction ]
     "U": [(-1, 0),"R"], 
-    "D": [(1, 0),"L"],  
+    "D": [(1, 0),"L"],      
     "L": [(0, -1),"U"],  
     "R": [(0, 1),"D"]
 }
@@ -48,8 +48,44 @@ def distinct_positions(map):
             positions.add((x,y))
         next_pos = (x + directions[direction][0][0], y + directions[direction][0][1])
     
-    return len(positions)
+    return positions
         
-print(f'Part 1: {distinct_positions(map)}')
+print(f'Part 1: {len(distinct_positions(map))}')
 
 # Part 2
+
+def check_loop(map):
+    positions = set()
+    x, y = find_guard(map)
+    direction = "U"
+    positions.add((x,y,direction))
+    next_pos = (x + directions[direction][0][0], y + directions[direction][0][1])
+
+    while 0 <= next_pos[0] < len(map) and 0 <= next_pos[1] < len(map):
+        while map[next_pos[0]][next_pos[1]] == "#":
+            direction = directions[direction][1]
+            next_pos = (x + directions[direction][0][0], y + directions[direction][0][1])
+        dx, dy = directions[direction][0]
+        x += dx
+        y += dy
+        if (x,y,direction) in positions:
+            return True
+        else:
+            positions.add((x,y,direction))
+        next_pos = (x + dx, y + dy)
+
+    return False
+
+
+def number_blocks(map):
+    sum = 0
+    positions = distinct_positions(map)
+    for j, i in positions:
+        if map[j][i] == '.':
+            map[j] = map[j][:i] + '#' + map[j][i+1:]
+            if check_loop(map):
+                sum += 1
+            map[j] = map[j][:i] + '.' + map[j][i+1:]
+    return sum
+
+print(f'Part 2: {number_blocks(map)}')
